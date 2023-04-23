aef80aa7e74c14220f71e4fe4903bfdaf0ae0fd3
moejehad <mohammedmadhoun18@gmail.com>
2023-04-23 21:09:07 +0300

    First Commit

---
+++ app/src/main/java/com/moejehad/reelsapp/helpers/Constants.kt
@@ -0,0 +1,47 @@
+package com.moejehad.reelsapp.helpers
+
+object Constants {
+
+    val videoList = listOf(
+        "https://vod-progressive.akamaized.net/exp=1682282514~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2851%2F11%2F289258217%2F1094756920.mp4~hmac=00425d1e10b3de704e068fd5074cd0851e1199985e339e5d733e8b46ff8c77f2/vimeo-prod-skyfire-std-us/01/2851/11/289258217/1094756920.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282565~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1877%2F18%2F459389137%2F2030603908.mp4~hmac=e42d6b01a25d039828942764f737c261fe7269443fb65597cb5fc7c6f19f39dc/vimeo-prod-skyfire-std-us/01/1877/18/459389137/2030603908.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282581~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3418%2F20%2F517090081%2F2403276533.mp4~hmac=f282aa51d276e60d2ff6110cb162b806ceda3942428bde0a7103d4f2aa8e0454/vimeo-prod-skyfire-std-us/01/3418/20/517090081/2403276533.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282597~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F1868%2F21%2F534342299%2F2524499968.mp4~hmac=1bc6f300a2077b9649638ac1c0e2e8a4701878aca6e200738cab031b7663581d/vimeo-prod-skyfire-std-us/01/1868/21/534342299/2524499968.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282613~acl=%2Fvimeo-transcode-storage-prod-us-east1-h264-1440p%2F01%2F1082%2F13%2F330412624%2F1297186312.mp4~hmac=139d67f64bfb9a8f28e476d9f92e623a1aeb35c54d78ee5fa28ff8160177e4e4/vimeo-transcode-storage-prod-us-east1-h264-1440p/01/1082/13/330412624/1297186312.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282629~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2215%2F9%2F236075858%2F840331842.mp4~hmac=d440968193b9639b05369bf0632cfc866c9e9b9bcadf6811782c60dae52a7108/vimeo-prod-skyfire-std-us/01/2215/9/236075858/840331842.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282647~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F337%2F9%2F226685105%2F796795643.mp4~hmac=dd269c065eb2bb2dd02f6f2a6bd43b9cac14706689b9b15819e6e327f37146fd/vimeo-prod-skyfire-std-us/01/337/9/226685105/796795643.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282668~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4093%2F14%2F370467553%2F1536339768.mp4~hmac=61eb7098fb1787c56da6c014a57c353e58949669dc2c2433bc36e8ce2e05e9f5/vimeo-prod-skyfire-std-us/01/4093/14/370467553/1536339768.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282685~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2909%2F7%2F189545487%2F628513855.mp4~hmac=d301c41ac1f32c2b09199116fe6969c25947c23dbf47dcf778392802b972b383/vimeo-prod-skyfire-std-us/01/2909/7/189545487/628513855.mp4",
+        "https://vod-progressive.akamaized.net/exp=1682282697~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3994%2F10%2F269971860%2F1002284853.mp4~hmac=38d8e26845260e3898d9749d16cfee1137cd3cc86edd8feb4167e692d767a796/vimeo-prod-skyfire-std-us/01/3994/10/269971860/1002284853.mp4",
+        "https://player.vimeo.com/external/296210754.sd.mp4?s=9db41d71fa61a2cc19757f656fc5c5c5ef9f69ec&profile_id=164&oauth2_token_id=57447761",
+    )
+
+    val imageList = listOf(
+        "https://apod.nasa.gov/apod/image/1912/M27_Mazlin_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/ElectricMilkyWay_Pedretti_1080.jpg",
+        "https://apod.nasa.gov/apod/image/1912/NGC6744_FinalLiuYuhang1024.jpg",
+        "https://apod.nasa.gov/apod/image/1912/TaurusAbolfath1024.jpg",
+        "https://apod.nasa.gov/apod/image/1912/LinesOfTimeKomlev1100.jpg",
+        "https://apod.nasa.gov/apod/image/1912/geminids2013_beletsky_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/StarlinkTrails_Filter_1080.jpg",
+        "https://apod.nasa.gov/apod/image/1912/N63A_HubbleChandraSchmidt_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/m78ldn1622barnardsloopJulio1100.jpg",
+        "https://apod.nasa.gov/apod/image/1912/gem16otjcc1100.jpg",
+        "https://apod.nasa.gov/apod/image/1912/borisovStsci1315.jpg",
+        "https://apod.nasa.gov/apod/image/1912/mammatus_olson_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/M77Bfield_NASA_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/Horsehead_Hanson_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/J0030_NICER_1024.jpg",
+        "https://apod.nasa.gov/apod/image/1912/AS17-149-22859-2v2SmlWmk1024.jpg",
+        "https://apod.nasa.gov/apod/image/1912/N2616c_600h.jpg",
+        "https://apod.nasa.gov/apod/image/1912/WinterStars_Slovinsky_1080.jpg",
+        "https://apod.nasa.gov/apod/image/1912/AnnularEclipse_Pinski_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/Orava_Duskova_WinterHexagon_0.jpg",
+        "https://apod.nasa.gov/apod/image/1912/PartialSolar_Ghohroodi_960.jpg",
+        "https://apod.nasa.gov/apod/image/1912/DistortedSunrise_Chasiotis_1080.jpg",
+        "https://apod.nasa.gov/apod/image/1912/saturnplane_cassini_1004.jpg",
+        "https://apod.nasa.gov/apod/image/1912/M20_volskiy1024.jpg",
+        "https://apod.nasa.gov/apod/image/1912/M33-HaLRGB-RayLiao1024.jpg",
+    )
+
+}
---
+++ app/src/main/java/com/moejehad/reelsapp/presentation/reels/VideoPlayer.kt
@@ -0,0 +1,48 @@
+package com.moejehad.reelsapp.presentation.reels
+
+import android.widget.FrameLayout
+import android.widget.ListPopupWindow.MATCH_PARENT
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
+
+@Composable
+fun VideoPlayer(link : String) {
+
+    val context = LocalContext.current
+
+    val exoPlayer = ExoPlayer.Builder(context)
+        .build()
+        .also { exoPlayer ->
+            val mediaItem = MediaItem.Builder()
+                .setUri(link)
+                .build()
+            exoPlayer.setMediaItem(mediaItem)
+            exoPlayer.playWhenReady = true
+            exoPlayer.repeatMode = Player.REPEAT_MODE_ALL
+            exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+            exoPlayer.prepare()
+        }
+
+
+    DisposableEffect(
+        AndroidView(factory = {
+            StyledPlayerView(context).apply {
+                useController = false
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                player = exoPlayer
+                layoutParams = FrameLayout.LayoutParams(
+                    MATCH_PARENT,MATCH_PARENT
+                )
+            }
+        })
+    ) {
+        onDispose { exoPlayer.release() }
+    }
+
+}
